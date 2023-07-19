b5a898c35bb862f5f4e993adfb7b50c42ba12e86
Steven J. Gibbons <74380921+stevenjgibbons@users.noreply.github.com>
2023-07-19 10:48:03 +0200

    Add files via upload

---
+++ DPRK_dataset/calculate_DPRK_theoretical_slovecs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+python ../statphase2slowvec.py  \
+         --reflat  41.295     \
+         --reflon 129.080     \
+         --phaselistfile    DPRK_phaselist.txt   \
+         --outfile          DPRK_ak135_slovecs.txt
